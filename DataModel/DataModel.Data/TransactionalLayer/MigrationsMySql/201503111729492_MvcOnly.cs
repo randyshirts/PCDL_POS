66971754b21ff3d05b43c23f4c9d465f961b7b0e
randyshirts <randy.shirts@gmail.com>
2015-03-11 16:50:19 -0500

    20150311

---
+++ DataModel/DataModel.Data/TransactionalLayer/MigrationsMySql/201503111729492_MvcOnly.cs
@@ -0,0 +1,16 @@
+namespace DataModel.Data.TransactionalLayer.MigrationsMySql
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class MvcOnly : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
